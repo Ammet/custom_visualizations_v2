42a271ca740ecd3aee0c259b7f002dbd05154049
Taras Vlasenko <taras@dvelp.co.uk>
2021-04-12 17:15:27 +0300

    Fix warnings, make gradient look nicer

--- src/examples/sankey/sankey.ts
+++ src/examples/sankey/sankey.ts
@@ -67,10 +67,10 @@ const vis: Sankey = {
     const height = element.clientHeight
 
     const svg = this.svg
-      .html('')
-      .attr('width', '100%')
-      .attr('height', '100%')
-      .append('g')
+        .html('')
+        .attr('width', '100%')
+        .attr('height', '100%')
+        .append('g')
 
     const dimensions = queryResponse.fields.dimension_like
     const measure = queryResponse.fields.measure_like[0]
@@ -79,31 +79,31 @@ const vis: Sankey = {
     // `no-inferred-empty-object-type  Explicit type parameter needs to be provided to the function call`
     // https://stackoverflow.com/questions/31564730/typescript-with-d3js-with-definitlytyped
     const color = d3.scaleOrdinal<string[], string[]>()
-      .range(config.color_range || vis.options.color_range.default)
+        .range(config.color_range || vis.options.color_range.default)
 
     const defs = svg.append('defs')
 
     const sankeyInst = sankey()
-      .nodeAlign(sankeyLeft)
-      .nodeWidth(10)
-      .nodePadding(12)
-      .extent([[1, 1], [width - 1, height - 6]])
+        .nodeAlign(sankeyLeft)
+        .nodeWidth(10)
+        .nodePadding(12)
+        .extent([[1, 1], [width - 1, height - 6]])
 
     // TODO: Placeholder until @types catches up with sankey
     const newSankeyProps: any = sankeyInst
     newSankeyProps.nodeSort(null)
 
     let link = svg.append('g')
-      .attr('class', 'links')
-      .attr('fill', 'none')
-      .attr('stroke', '#fff')
-      .selectAll('path')
+        .attr('class', 'links')
+        .attr('fill', 'none')
+        .attr('stroke', '#fff')
+        .selectAll('path')
 
     let node = svg.append('g')
-      .attr('class', 'nodes')
-      .attr('font-family', 'sans-serif')
-      .attr('font-size', 10)
-      .selectAll('g')
+        .attr('class', 'nodes')
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', 10)
+        .selectAll('g')
 
     const graph: any = {
       nodes: [],
@@ -111,11 +111,11 @@ const vis: Sankey = {
     }
 
     const nodes = d3.set()
-    var pathId: any = 0
+    let pathId: any = 0
 
     data.forEach(function (d: any) {
       // variable number of dimensions
-      var path: any[] = []
+      let path: any[] = []
       pathId += 1
 
       for (const dim of dimensions) {
@@ -164,126 +164,127 @@ const vis: Sankey = {
     sankeyInst(graph)
 
     link = link
-      .data(graph.links)
-      .enter().append('path')
-      .attr('class', 'link')
-      .attr('d', function (d: any) { return 'M' + -10 + ',' + -10 + sankeyLinkHorizontal()(d) })
-      .style('opacity', 0.4)
-      .attr('stroke-width', function (d: Cell) { return Math.max(1, d.width) })
-      .on('mouseenter', function (this: any, d: Cell) {
-        svg.selectAll('.link').style('opacity', 0.05)
+        .data(graph.links)
+        .enter().append('path')
+        .attr('class', 'link')
+        .attr('d', function (d: any) { return 'M' + -10 + ',' + -10 + sankeyLinkHorizontal()(d) })
+        .style('opacity', 0.4)
+        .attr('stroke-width', function (d: Cell) { return Math.max(1, d.width) })
+        .on('mouseenter', function (this: any, d: Cell) {
+          svg.selectAll('.link').style('opacity', 0.05)
 
-        var activeNodes: any[] = [d.source, d.target]
+          const activeNodes: any[] = [d.source, d.target]
 
-        svg.selectAll('.link').style('opacity', function (p: any) {
-          if (p.pathId == d.pathId) {
-            activeNodes.push(p.source)
-            activeNodes.push(p.target)
-            return 0.7
-          }
-          return 0.05
+          svg.selectAll('.link').style('opacity', function (p: any) {
+
+            if (p.pathId === d.pathId) {
+              activeNodes.push(p.source)
+              activeNodes.push(p.target)
+              return 0.7
+            }
+            return 0.05
+          })
+
+          svg.selectAll('.node').style('opacity', function (p: any) {
+            if (activeNodes.includes(p)) return 1
+            return 0.5
+          })
+
+          d3.select(this).style('opacity', 0.7)
         })
-
-        svg.selectAll('.node').style('opacity', function (p: any) {
-          if (activeNodes.includes(p)) return 1
-          return 0.5
+        .on('click', function (this: any, d: Cell) {
+          // Add drill menu event
+          const coords = d3.mouse(this)
+          const event: object = { pageX: coords[0], pageY: coords[1] }
+          LookerCharts.Utils.openDrillMenu({
+            links: d.drillLinks,
+            event: event
+          })
         })
-
-        d3.select(this).style('opacity', 0.7)
-      })
-      .on('click', function (this: any, d: Cell) {
-        // Add drill menu event
-        const coords = d3.mouse(this)
-        const event: object = { pageX: coords[0], pageY: coords[1] }
-        LookerCharts.Utils.openDrillMenu({
-          links: d.drillLinks,
-          event: event
+        .on('mouseleave', function (d: Cell) {
+          d3.selectAll('.node').style('opacity', 1)
+          d3.selectAll('.link').style('opacity', 0.4)
         })
-      })
-      .on('mouseleave', function (d: Cell) {
-        d3.selectAll('.node').style('opacity', 1)
-        d3.selectAll('.link').style('opacity', 0.4)
-      })
 
     // gradients https://bl.ocks.org/micahstubbs/bf90fda6717e243832edad6ed9f82814
     link.style('stroke', function (d: Cell, i: number) {
       // make unique gradient ids
       const gradientID = 'gradient' + i
 
-      var startColor = color(d.source.name);
-      var stopColor = color(d.target.name);
+      const startColor = color(d.source.name)
+      const stopColor = color(d.target.name)
 
       const linearGradient = defs.append('linearGradient')
-        .attr('id', gradientID)
-        .attr("x1", d.source.x0)
-        .attr("y1", d.source.y0)
-        .attr("x2", d.target.x0)
-        .attr("y2", d.target.y0)
-        .attr("gradientUnits", "userSpaceOnUse")
+          .attr('id', gradientID)
+          .attr('x1', d.source.x0)
+          .attr('y1', 0)
+          .attr('x2', d.target.x0)
+          .attr('y2', 0)
+          .attr('gradientUnits', 'userSpaceOnUse')
 
       linearGradient.selectAll('stop')
-        .data([
-          { offset: '10%', color: startColor },
-          { offset: '90%', color: stopColor }
-        ])
-        .enter().append('stop')
-        .attr('offset', function (d: Cell) {
-          return d.offset
-        })
-        .attr('stop-color', function (d: Cell) {
-          return d.color
-        })
+          .data([
+            { offset: '10%', color: startColor },
+            { offset: '90%', color: stopColor }
+          ])
+          .enter().append('stop')
+          .attr('offset', function (d: Cell) {
+            return d.offset
+          })
+          .attr('stop-color', function (d: Cell) {
+            return d.color
+          })
 
-      return 'url(#' + gradientID + ')'
+      return `url(#${gradientID})`
     })
 
     node = node
-      .data(graph.nodes)
-      .enter().append('g')
-      .attr('class', 'node')
-      .on('mouseenter', function (d: Cell) {
-        svg.selectAll('.link')
-          .style('opacity', function (p: any) {
-            if (p.source === d) return 0.7
-            if (p.target === d) return 0.7
-            return 0.05
-          })
-      })
-      .on('mouseleave', function (d: Cell) {
-        d3.selectAll('.link').style('opacity', 0.4)
-      })
+        .data(graph.nodes)
+        .enter().append('g')
+        .attr('class', 'node')
+        .on('mouseenter', function (d: Cell) {
+          svg.selectAll('.link')
+              .style('opacity', function (p: any) {
+                if (p.source === d) return 0.7
+                if (p.target === d) return 0.7
+                return 0.05
+              })
+        })
+        .on('mouseleave', function (d: Cell) {
+          d3.selectAll('.link').style('opacity', 0.4)
+        })
 
     node.append('rect')
-      .attr('x', function (d: Cell) { return d.x0 })
-      .attr('y', function (d: Cell) { return d.y0 })
-      .attr('height', function (d: Cell) { return Math.abs(d.y1 - d.y0) })
-      .attr('width', function (d: Cell) { return Math.abs(d.x1 - d.x0) })
-      .attr('fill', function (d: Cell) { return d.color = color(d.name) })
-      .attr('stroke', '#555')
+        .attr('x', function (d: Cell) { return d.x0 })
+        .attr('y', function (d: Cell) { return d.y0 })
+        .attr('height', function (d: Cell) { return Math.abs(d.y1 - d.y0) })
+        .attr('width', function (d: Cell) { return Math.abs(d.x1 - d.x0) })
+        .attr('fill', function (d: Cell) { return d.color = color(d.name) })
+        .attr('stroke', '#555')
 
     node.append('text')
-      .attr('x', function (d: Cell) { return d.x0 - 6 })
-      .attr('y', function (d: Cell) { return (d.y1 + d.y0) / 2 })
-      .attr('dy', '0.35em')
-      .style('font-weight', 'bold')
-      .attr('text-anchor', 'end')
-      .style('fill', '#222')
-      .text(function (d: Cell) {
-        switch (config.label_type) {
-          case 'name':
-            return d.name
-          case 'name_value':
-            return `${d.name} (${d.value})`
-          default:
-            return ''
-        }
-      })
-      .filter(function (d: Cell) { return d.x0 < width / 2 })
-      .attr('x', function (d: Cell) { return d.x1 + 6 })
-      .attr('text-anchor', 'start')
+        .attr('x', function (d: Cell) { return d.x0 - 6 })
+        .attr('y', function (d: Cell) { return (d.y1 + d.y0) / 2 })
+        .attr('dy', '0.35em')
+        .style('font-weight', 'bold')
+        .attr('text-anchor', 'end')
+        .style('fill', '#222')
+        .text(function (d: Cell) {
+          switch (config.label_type) {
+            case 'name':
+              return d.name
+            case 'name_value':
+              return `${d.name} (${d.value})`
+            default:
+              return ''
+          }
+        })
+        .filter(function (d: Cell) { return d.x0 < width / 2 })
+        .attr('x', function (d: Cell) { return d.x1 + 6 })
+        .attr('text-anchor', 'start')
 
     node.append('title')
-      .text(function (d: Cell) { return d.name + '\n' + d.value })
+        .text(function (d: Cell) { return d.name + '\n' + d.value })
     doneRendering()
   }
 }
